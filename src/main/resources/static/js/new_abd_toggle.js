331f41d0f684a6f3b4e94c9d8c2bed7cb588b7cf
Alexandr Ludov <alesant999@gmail.com>
2024-09-29 23:10:35 +0300

    Добавлены приказы на представителей проектировщика

--- src/main/resources/static/js/new_abd_toggle.js
+++ src/main/resources/static/js/new_abd_toggle.js
@@ -82,30 +82,83 @@ function addSubcustomerResponsible() {
             <h4 class="h4 mb-0 text-gray-800">Представитель подрядчика №1</h4>
         </div>
         <div class="form-group row">
-            <label for="subcustomerResponsiblePosition_1" class="col-sm-4">Должность представителя подрядчика:</label>
-            <input type="text" id="subcustomerResponsiblePosition_1" name="subcustomerResponsiblePosition_1" required class="col-sm-8">
+            <label for="subcustomerResponsiblePosition_${responsibleCounter}" class="col-sm-4">Должность представителя подрядчика:</label>
+            <input type="text" id="subcustomerResponsiblePosition_${responsibleCounter}" name="subcustomerResponsiblePosition_${responsibleCounter}" required class="col-sm-8">
         </div>
         <div class="form-group row">
-            <label for="subcustomerResponsibleName_1" class="col-sm-4">ФИО представителя подрядчика:</label>
-            <input type="text" id="subcustomerResponsibleName_1" name="subcustomerResponsibleName_1" required class="col-sm-8">
+            <label for="subcustomerResponsibleName_${responsibleCounter}" class="col-sm-4">ФИО представителя подрядчика:</label>
+            <input type="text" id="subcustomerResponsibleName_${responsibleCounter}" name="subcustomerResponsibleName_${responsibleCounter}" required class="col-sm-8">
         </div>
         <div class="form-group row">
-            <label for="subcustomerResponsibleID_1" class="col-sm-6">Идентификационный номер в национальном реестре специалистов:</label>
-            <input type="text" id="subcustomerResponsibleID_1" name="subcustomerResponsibleID_1" required class="col-sm-6">
+            <label for="subcustomerResponsibleID_${responsibleCounter}" class="col-sm-6">Идентификационный номер в национальном реестре специалистов:</label>
+            <input type="text" id="subcustomerResponsibleID_${responsibleCounter}" name="subcustomerResponsibleID_${responsibleCounter}" required class="col-sm-6">
         </div>
         <div class="form-group row">
-            <label for="subcustomerResponsibleDocument_1" class="col-sm-4">Распорядительный документ:</label>
-            <input type="text" id="subcustomerResponsibleDocument_1" name="subcustomerResponsibleDocument_1" required class="col-sm-8">
+            <label for="subcustomerResponsibleDocument_${responsibleCounter}" class="col-sm-4">Распорядительный документ:</label>
+            <input type="text" id="subcustomerResponsibleDocument_${responsibleCounter}" name="subcustomerResponsibleDocument_${responsibleCounter}" required class="col-sm-8">
         </div>
         <div class="form-group row">
-            <label for="subcustomerResponsibleNumber_1" class="col-sm-4">Номер распорядительного документа:</label>
-            <input type="text" id="subcustomerResponsibleNumber_1" name="subcustomerResponsibleNumber_1" required class="col-sm-8">
+            <label for="subcustomerResponsibleNumber_${responsibleCounter}" class="col-sm-4">Номер распорядительного документа:</label>
+            <input type="text" id="subcustomerResponsibleNumber_${responsibleCounter}" name="subcustomerResponsibleNumber_${responsibleCounter}" required class="col-sm-8">
         </div>
         <div class="form-group row">
-            <label for="subcustomerResponsibleDate_1" class="col-sm-4">Дата распорядительного документа:</label>
-            <input type="text" id="subcustomerResponsibleDate_1" name="subcustomerResponsibleDate_1" required class="col-sm-8">
+            <label for="subcustomerResponsibleDate_${responsibleCounter}" class="col-sm-4">Дата распорядительного документа:</label>
+            <input type="text" id="subcustomerResponsibleDate_${responsibleCounter}" name="subcustomerResponsibleDate_${responsibleCounter}" required class="col-sm-8">
         </div>
     `;
 
     container.appendChild(newResponsible);
 }
+
+// Функция для добавления полей "Ответственное лицо"
+ function addDesignerResponsible() {
+     responsibleCounter++;
+
+     const container = document.getElementById('designerResponsiblesContainer');
+     const newResponsible = document.createElement('div');
+     newResponsible.classList.add('form-group', 'mb-4');
+
+     newResponsible.innerHTML = `
+         <div class="d-sm-flex align-items-center justify-content-between mb-4">
+             <h4 class="h4 mb-0 text-gray-800">Представитель проектировщика №1</h4>
+         </div>
+         <div class="form-group row">
+             <label for="designerResponsiblePosition_${responsibleCounter}" class="col-sm-4">Должность представителя проектировщика:</label>
+             <input type="text" id="designerResponsiblePosition_${responsibleCounter}" name="designerResponsiblePosition_${responsibleCounter}" required class="col-sm-8">
+         </div>
+         <div class="form-group row">
+             <label for="designerResponsibleName_${responsibleCounter}" class="col-sm-4">ФИО представителя проектировщика:</label>
+             <input type="text" id="designerResponsibleName_${responsibleCounter}" name="designerResponsibleName_${responsibleCounter}" required class="col-sm-8">
+         </div>
+         <div class="form-group row">
+             <label for="designerResponsibleDocument_1" class="col-sm-4">Распорядительный документ:</label>
+             <input type="text" id="designerResponsibleDocument_1" name="designerResponsibleDocument_1" required class="col-sm-8">
+         </div>
+         <div class="form-group row">
+             <label for="designerResponsibleNumber_${responsibleCounter}" class="col-sm-4">Номер распорядительного документа:</label>
+             <input type="text" id="designerResponsibleNumber_${responsibleCounter}" name="designerResponsibleNumber_${responsibleCounter}" required class="col-sm-8">
+         </div>
+         <div class="form-group row">
+             <label for="designerResponsibleDate_${responsibleCounter}" class="col-sm-4">Дата распорядительного документа:</label>
+             <input type="text" id="designerResponsibleDate_${responsibleCounter}" name="designerResponsibleDate_${responsibleCounter}" required class="col-sm-8">
+         </div>
+         <div class="form-group row">
+             <label for="designerResponsibleCompany_${responsibleCounter}" class="col-sm-6">Юридическое лицо представителя проектировщика:</label>
+             <input type="text" id="designerResponsibleCompany_${responsibleCounter}" name="designerResponsibleCompany_${responsibleCounter}" required class="col-sm-6">
+         </div>
+         <div class="form-group row">
+             <label for="designerResponsibleOGRN_${responsibleCounter}" class="col-sm-4">ОГРН юридического лица:</label>
+             <input type="text" id="designerResponsibleOGRN_${responsibleCounter}" name="designerResponsibleOGRN_${responsibleCounter}" required class="col-sm-8">
+         </div>
+         <div class="form-group row">
+             <label for="designerResponsibleINN_${responsibleCounter}" class="col-sm-4">ИНН юридического лица:</label>
+             <input type="text" id="designerResponsibleINN_${responsibleCounter}" name="designerResponsibleINN_${responsibleCounter}" required class="col-sm-8">
+         </div>
+         <div class="form-group row">
+             <label for="designerResponsibleAddress_${responsibleCounter}" class="col-sm-4">Адрес юридического лица:</label>
+             <input type="text" id="designerResponsibleAddress_${responsibleCounter}" name="designerResponsibleAddress_${responsibleCounter}" required class="col-sm-8">
+         </div>
+     `;
+
+     container.appendChild(newResponsible);
+ }
