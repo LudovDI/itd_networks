6b642aedf31bbaa3be7f59b97c2ca6e29cee52ac
Alexandr Ludov <alesant999@gmail.com>
2024-09-30 17:38:21 +0300

    Добавлен шаблон для страницы информации об материалах

--- src/main/java/com/automation_of_ITD_formation/Automation/of/ITD/formation/controllers/TemplateController.java
+++ src/main/java/com/automation_of_ITD_formation/Automation/of/ITD/formation/controllers/TemplateController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NewABDController {
+public class TemplateController {
 
     @GetMapping("/new_abd")
     public String index() {
         return "new_abd";
+    }
+
+    @GetMapping("/materials_used")
+    public String materials() {
+        return "materials_used";
     }
 }
